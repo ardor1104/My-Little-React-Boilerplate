382e8f18e5863b810efe9961da33bf58c33093fc
ardor1104 <87302777+ardor1104@users.noreply.github.com>
2023-02-13 16:10:32 +0900

    feature: 🎉 logger util

---
+++ src/utils/logger/index.ts
@@ -0,0 +1,24 @@
+// thx sehvdi
+import { MODE } from '../../../config';
+
+const isDevelopmentMode = MODE === 'development';
+
+const logger = {
+  log(...args: any[]) {
+    if (isDevelopmentMode) {
+      console.log(...args);
+    }
+  },
+  error(...args: any[]) {
+    if (isDevelopmentMode) {
+      console.error(...args);
+    }
+  },
+  warn(...args: any[]) {
+    if (isDevelopmentMode) {
+      console.warn(...args);
+    }
+  },
+};
+
+export default logger;
