fb0486e554a7c242d94a4459011169d946c3a203
ardor1104 <87302777+ardor1104@users.noreply.github.com>
2023-02-15 16:15:11 +0900

    feature: ✨ add action

--- src/components/atoms/Button/index.stories.tsx
+++ src/components/atoms/Button/index.stories.tsx
@@ -2,6 +2,7 @@ import Button from '.';
 
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import { withKnobs, select, text, boolean } from '@storybook/addon-knobs';
+import { action } from '@storybook/addon-actions';
 
 const kindOptions = {
   mono: 'mono',
@@ -17,9 +18,12 @@ const sizeOptions = {
 };
 
 export default {
-  title: 'Button',
+  title: 'atoms/Button',
   component: Button,
   decorators: [withKnobs],
+  parameters: {
+    componentSubtitle: `베이스가 되는 버튼 컴포넌트`,
+  },
 } as ComponentMeta<typeof Button>;
 
 export const Default: ComponentStory<typeof Button> = () => {
@@ -45,6 +49,8 @@ export const Default: ComponentStory<typeof Button> = () => {
       to={emptyTextConverter(to)}
       href={emptyTextConverter(href)}
       isDisabled={isDisabled}
+      onClick={action('onClick')}
+      disabledOnClick={action('disabledOnClick')}
     >
       {value}
     </Button>
