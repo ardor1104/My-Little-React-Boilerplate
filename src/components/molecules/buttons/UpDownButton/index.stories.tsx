f47cbdab74ec00dd9731e9af37c3a3eaa3203314
ardor1104 <87302777+ardor1104@users.noreply.github.com>
2023-02-15 16:30:43 +0900

    feature: ✨ add action

--- src/components/molecules/buttons/UpDownButton/index.stories.tsx
+++ src/components/molecules/buttons/UpDownButton/index.stories.tsx
@@ -2,17 +2,25 @@ import UpDownButton from '.';
 
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import { withKnobs, number } from '@storybook/addon-knobs';
+import { action } from '@storybook/addon-actions';
 
 export default {
-  title: 'UpDownButton',
+  title: 'molecules/buttons/UpDownButton',
   component: UpDownButton,
   decorators: [withKnobs],
 } as ComponentMeta<typeof UpDownButton>;
 
 export const Default: ComponentStory<typeof UpDownButton> = () => {
   const propNumber = number('propNumber', 0);
-  const max = number('max', 0);
+  const max = number('max', 10);
   const min = number('min', 0);
 
-  return <UpDownButton propNumber={propNumber} max={max} min={min} />;
+  return (
+    <UpDownButton
+      propNumber={propNumber}
+      max={max}
+      min={min}
+      onChange={action(`onChnage`)}
+    />
+  );
 };
